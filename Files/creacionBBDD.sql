4fa071bc9c6477119a8aa2c9506ca43bf4a4b0b6
lopezleyre <leyre.ibz@gmail.com>
2024-09-15 12:22:48 +0200

    gitignore, fase 3 a falta de tabla union y algo de historias de usuario fase 3

--- Files/creacionBBDD.sql
+++ Files/creacionBBDD.sql
@@ -5,21 +5,21 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 -- -----------------------------------------------------
--- Schema mydb
+-- Schema abccorporation
 -- -----------------------------------------------------
 
 -- -----------------------------------------------------
--- Schema mydb
+-- Schema abccorporation
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `ABCCorporation (M3T3)` DEFAULT CHARACTER SET utf8 ;
-USE `mydb` ;
+CREATE SCHEMA IF NOT EXISTS `abccorporation` DEFAULT CHARACTER SET utf8 ;
+USE `abccorporation` ;
 
 -- -----------------------------------------------------
--- Table `mydb`.`PERSONAL_INFO`
+-- Table `abccorporation`.`PERSONAL_INFO`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`PERSONAL_INFO` (
+CREATE TABLE IF NOT EXISTS `abccorporation`.`PERSONAL_INFO` (
   `IdEmployee` INT NOT NULL,
-  `EmployeeNumber` INT NULL,
+  `EmployeeNumber` VARCHAR(45) NULL,
   `Age` INT NULL,
   `DateBirth` INT NULL,
   `Gender` VARCHAR(45) NULL,
@@ -32,10 +32,10 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`EMPLOYEE_SATISFACTION`
+-- Table `abccorporation`.`EMPLOYEE_SATISFACTION`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`EMPLOYEE_SATISFACTION` (
-  `IdEmployeeSatisfaction` INT NOT NULL,
+CREATE TABLE IF NOT EXISTS `abccorporation`.`EMPLOYEE_SATISFACTION` (
+  `IdEmployeeSatisfaction` INT NOT NULL AUTO_INCREMENT,
   `EnvironmentSatisfaction` INT NULL,
   `JobInvolvement` INT NULL,
   `JobSatisfaction` INT NULL,
@@ -46,55 +46,59 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`JOB_DATA`
+-- Table `abccorporation`.`JOB_DATA`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`JOB_DATA` (
-  `IdJobData` INT NOT NULL,
+CREATE TABLE IF NOT EXISTS `abccorporation`.`JOB_DATA` (
+  `IdJobData` INT NOT NULL AUTO_INCREMENT,
   `Attrition` VARCHAR(45) NULL,
   `JobLevel` INT NULL,
   `JobRole` VARCHAR(100) NULL,
   `NumCompaniesWorked` INT NULL,
-  `TotalWorkingYears` INT NULL,
+  `TotalWorkingYears` VARCHAR(45) NULL,
   `YearsAtCompany` INT NULL,
   `YearsSinceLastPromotion` INT NULL,
   `YearsWithCurrManager` INT NULL,
+  `JOB_DATAcol` VARCHAR(45) NULL,
   PRIMARY KEY (`IdJobData`))
-ENGINE = InnoDB;
+ENGINE = InnoDB
+COMMENT = '\n';
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`SALARY`
+-- Table `abccorporation`.`SALARY`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`SALARY` (
-  `IdSalary` INT NOT NULL,
+CREATE TABLE IF NOT EXISTS `abccorporation`.`SALARY` (
+  `IdSalary` INT NOT NULL AUTO_INCREMENT,
   `HourlyRate` FLOAT NULL,
   `DailyRate` FLOAT NULL,
   `MonthlyRate` FLOAT NULL,
-  `MonthlyIncome` FLOAT NULL,
   `PercentSalaryHike` INT NULL,
+  `SALARYcol` VARCHAR(45) NULL,
   PRIMARY KEY (`IdSalary`))
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`JOB_EXTRAS`
+-- Table `abccorporation`.`JOB_EXTRAS`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`JOB_EXTRAS` (
-  `IdJobExtras` INT NOT NULL,
+CREATE TABLE IF NOT EXISTS `abccorporation`.`JOB_EXTRAS` (
+  `IdJobExtras` INT NOT NULL AUTO_INCREMENT,
   `OverTime` VARCHAR(45) NULL,
   `BusinessTravel` VARCHAR(45) NULL,
   `StockOptionLevel` INT NULL,
   `TrainingTimesLastYear` INT NULL,
   `RemoteWork` TINYINT NULL,
+  `PerformanceRating` INT NULL,
+  `JOB_EXTRAScol` VARCHAR(45) NULL,
   PRIMARY KEY (`IdJobExtras`))
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`UNION`
+-- Table `abccorporation`.`UNION`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`UNION` (
-  `IdUnion` INT NOT NULL,
+CREATE TABLE IF NOT EXISTS `abccorporation`.`UNION` (
+  `IdUnion` INT NOT NULL AUTO_INCREMENT,
   `PERSONAL_INFO_IdEmployee` INT NOT NULL,
   `EMPLOYEE_SATISFACTION_IdEmployeeSatisfaction` INT NOT NULL,
   `JOB_DATA_IdJobData` INT NOT NULL,
@@ -108,27 +112,27 @@ CREATE TABLE IF NOT EXISTS `mydb`.`UNION` (
   INDEX `fk_UNION_JOB_EXTRAS1_idx` (`JOB_EXTRAS_IdJobExtras` ASC) VISIBLE,
   CONSTRAINT `fk_UNION_PERSONAL_INFO`
     FOREIGN KEY (`PERSONAL_INFO_IdEmployee`)
-    REFERENCES `mydb`.`PERSONAL_INFO` (`IdEmployee`)
+    REFERENCES `abccorporation`.`PERSONAL_INFO` (`IdEmployee`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_UNION_EMPLOYEE_SATISFACTION1`
     FOREIGN KEY (`EMPLOYEE_SATISFACTION_IdEmployeeSatisfaction`)
-    REFERENCES `mydb`.`EMPLOYEE_SATISFACTION` (`IdEmployeeSatisfaction`)
+    REFERENCES `abccorporation`.`EMPLOYEE_SATISFACTION` (`IdEmployeeSatisfaction`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_UNION_JOB_DATA1`
     FOREIGN KEY (`JOB_DATA_IdJobData`)
-    REFERENCES `mydb`.`JOB_DATA` (`IdJobData`)
+    REFERENCES `abccorporation`.`JOB_DATA` (`IdJobData`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_UNION_SALARY1`
     FOREIGN KEY (`SALARY_IdSalary`)
-    REFERENCES `mydb`.`SALARY` (`IdSalary`)
+    REFERENCES `abccorporation`.`SALARY` (`IdSalary`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_UNION_JOB_EXTRAS1`
     FOREIGN KEY (`JOB_EXTRAS_IdJobExtras`)
-    REFERENCES `mydb`.`JOB_EXTRAS` (`IdJobExtras`)
+    REFERENCES `abccorporation`.`JOB_EXTRAS` (`IdJobExtras`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
--- historias-usuario.md
+++ historias-usuario.md
@@ -1,2 +1,6 @@
 HISTORIAS DE USUARIO
 
+### Fase 3.
+
+- Al insertar los datos tuvimos problemas a la hora de coger cada elemento del DataFrame y creamos una función para poder utilizarlos.
+- La tabla de unión de las claves primarias no se rellena automáticamente.
